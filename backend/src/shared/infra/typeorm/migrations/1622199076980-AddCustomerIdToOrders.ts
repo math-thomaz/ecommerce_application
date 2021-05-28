cfdd92b329ae09ca0a2fd99d0e61647a259c9b8c
Matheus Thomaz <matheustthomaz@gmail.com>
2021-05-28 09:11:16 -0300

    Project started! Basic structure and settings files added!

---
+++ backend/src/shared/infra/typeorm/migrations/1622199076980-AddCustomerIdToOrders.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddCustomerIdToOrders1622199076980 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
