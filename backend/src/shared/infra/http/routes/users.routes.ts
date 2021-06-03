54c951929df3f4b49e68b725bbb730bc6eae09e1
Matheus Thomaz <matheustthomaz@gmail.com>
2021-06-03 11:48:53 -0300

    New use cases added for user's module. Now its possible to see your user profile and import a .csv file to user's database.

--- backend/src/shared/infra/http/routes/users.routes.ts
+++ backend/src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,32 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ImportUserController } from "@modules/accounts/useCases/importUser/ImportUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
+
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
+const uploadPath = multer(uploadConfig);
+
 const createUserController = new CreateUserController();
+const importUserController = new ImportUserController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.post(
+  "/import",
+  uploadPath.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
+  importUserController.handle
+);
+
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
